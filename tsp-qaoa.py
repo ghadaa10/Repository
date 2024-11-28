73c6902c432a03b175fc08ddc209c169fc131858
ghadaa10 <121192670+ghadaa10@users.noreply.github.com>
2024-11-28 23:16:25 +0100

    Add files via upload

---
+++ tsp-qaoa.py
@@ -0,0 +1,73 @@
+import numpy as np
+import numpy.random as npr
+import matplotlib.pyplot as plt
+from qat.opt import CombinatorialProblem
+from qat.vsolve.ansatz import AnsatzFactory
+from qat.pylinalg import PyLinalg
+from qat.qpus import get_default_qpu
+from qat.clinalg import CLinalg
+from qat.plugins import ScipyMinimizePlugin
+from qat.plugins import SPSAMinimizePlugin
+from qat.core import Result
+from qat.core.qpu import QPUHandler
+from qat.core import Result
+from qat.core.wrappers.result import aggregate_data
+import time
+from qat.lang.AQASM import RX, X, H, Y, Z
+import time
+
+nb_villes = 3
+nbCouches = 10
+C = [[0, 1, 1], [1, 0, 1], [1, 1, 0]]
+constraint = max(C) * nb_villes
+problem = CombinatorialProblem("TSP", False)
+var_array = problem.new_vars(nb_villes**2)
+for i in range(nb_villes):
+    for j in range(nb_villes):
+        for t in range(nb_villes - 1):
+            problem.add_clause(
+                var_array[i + nb_villes * t]
+                & var_array[j + nb_villes * ((t + 1) % nb_villes)],
+                C[i][j],
+            )
+for t in range(nb_villes):
+    for i in range(nb_villes):
+        problem.add_clause(var_array[i + nb_villes * t], (-2) * 10)
+        for i2 in range(nb_villes):
+            if i2 != i:
+                problem.add_clause(
+                    var_array[i + nb_villes * t] & var_array[i2 + nb_villes * t],
+                    10,
+                )
+        
+        for t2 in range(nb_villes):
+            if t2 != t:
+                problem.add_clause(
+                    var_array[i + nb_villes * t] & var_array[i + nb_villes * t2],
+                    10,
+                )
+ansatz = problem.qaoa_ansatz(nbCouches)
+optimize = ScipyMinimizePlugin(
+    method="BFGS", tol=1e-5, options={"maxiter": 150}
+)  # L'optimiseur
+qpu = CLinalg()
+stack = (
+    optimize | qpu
+)  # on crée un stack : on va lancer le optimize sur le job qui sera soumis
+result = stack.submit(ansatz)  # résultat de l'optimisation
+circuit = ansatz.circuit.bind_variables(
+    result.parameter_map
+)  # On récupère le circuit précédent en
+# spécifiant les variables (qui sont celles de l'optimiseur)
+job = circuit.to_job(
+    nbshots=1000
+)  # Le second job, qui va nous permettre de connaître l'état du vecteur avec 10000 mesures
+qpu = CLinalg()
+result = qpu.submit(job)  # Résultat des mesures
+probasRes = np.zeros(2 ** (nb_villes**2))
+for sample in result:
+    print("State %s amplitude %s" % (sample.state, sample.probability))
+    bitstring = sample.state.bitstring
+    i = int(bitstring, base=2)
+    probasRes[i] = sample.probability
+    # s = format(i, 'b').zfill(N*M)
